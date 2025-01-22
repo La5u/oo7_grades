0da00e4f2aeb300f4694df6b9d15997d2914d021
la5u <lasushin@proton.me>
2025-01-22 17:42:38 +0500

    first commit

--- grades.py
+++ grades.py
@@ -1,24 +1,39 @@
 import urllib.request
 import pymupdf 
-from os import remove, system
+import os, os.path
 import re
+import sys
+
+def clear():
+    if sys.platform == 'linux':
+        os.system('clear')
+    else:
+        os.system('cls')
 
 FILENAME = 'grades.pdf'
-URL = input('url: ')
-# URL = 'http://outof7.nisa.edu.kz/parentsportal/gradebook_app.cfm?id=12345678'
+if not os.path.isfile('url.txt'):
 
-system('cls')
+    URL = input('url: ')
+    with open('url.txt', 'w') as f:
+        f.write(URL)
+
+else:
+    URL = open('url.txt').read()
+
+
 print('creating file...', end='\r')
 urllib.request.urlretrieve(URL, FILENAME)
+
 pdf = pymupdf.open(FILENAME)
 
+clear()
 for page in pdf: # iterate the document pages    
     text = page.get_text()
-    r = r'(.*) *\n[\S\s]*[12][A-G][ \n]*(.*)[\S\s]*KZ\n([\n\/\d%]*)%\n[\S\s]*Grade\n(\/[\n\/\d%]*)%\n[\S\s]*Grade\n([\d\n ]*)\n[\S\s]*Absence\n([\d\n]*)\n\d'
+    r = r'(.*) *\n[\S\s]*[12][A-G][ \n]*(.*)[\S\s]*KZ\n([\n\/\d%]*)%\n[\S\s]*Grade\n(\/[\n\/\d%]*)%\n[\S\s]*Grade\n([\d\n ]*)\n[\S\s]*'
     if 'KZ' not in text:
-        r = r'(.*) *\n[\S\s]*[12][A-G][ \n]*(.*)[\S\s]*?\/([\n\/\d%]*)%\n[\S\s]*Grade\n(\/[\n\/\d%]*)%\n[\S\s]*Grade\n([\d\n ]*)\n[\S\s]*Absence\n([\d\n]*)\n\d'
+        r = r'(.*) *\n[\S\s]*[12][A-G][ \n]*(.*)[\S\s]*?\/([\n\/\d%]*)%\n[\S\s]*Grade\n(\/[\n\/\d%]*)%\n[\S\s]*Grade\n([\d\n ]*)\n[\S\s]*'
     try:
-        name, subject, g1, g2, g3, attendance = re.search(r, text).groups()
+        name, subject, g1, g2, g3 = re.search(r, text).groups()
     except AttributeError:
         continue
     formative_max_grades = g1.replace('\n', '').replace('/', '').split('%')
@@ -26,7 +41,6 @@ for page in pdf: # iterate the document pages
     all_grades = g3.split('\n')
     formative_grades = all_grades[:len(formative_max_grades)*2+2]
     summative_grades = all_grades[len(formative_max_grades)*2+2:len(formative_max_grades)*2+2+len(summative_max_grades)*2+2]
-    attendance = attendance.split('\n')
 
     print(subject)
     print('\nTotal:')
@@ -47,15 +61,9 @@ for page in pdf: # iterate the document pages
     for grade, max_grade, percent in zip(summative_grades[::2], summative_max_grades, summative_grades[1::2]):
         print(f'{grade} / {max_grade} - {percent}%')
     print(f'{summative_grades[-2]}% - {summative_grades[-1]} ib')
-
-    
-
-
     input('(press enter to continue)')
+    clear()
 
-    system('cls')
 print('good luck,', name)
-
-
 
 
